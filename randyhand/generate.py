ef27af3d2115e5ef6936f26d9b070684de62d337
Austin <austin@growatomata.com>
2019-03-11 13:59:31 -0600

    Implements basic text generation & simple batch command.
    TODO:
    1. Get perspective transformations working (cant transform bounding boxes right).
    2. Refactor generate.py
    3. make randyhand command more robust

--- randyhand/generate.py
+++ randyhand/generate.py
@@ -38,6 +38,8 @@ from functools import partial, reduce
 import math
 import string
 import pandas as pd
+import xml.etree.cElementTree as ET
+
 
 
 def getGenerator(text=None, size=(500,500)):
@@ -56,13 +58,13 @@ def getGenerator(text=None, size=(500,500)):
     # next_word is a function
     next_word = get_next_word_function(text)
 
-    annotation_dict = {}
     emnist_dict = {}
     for index, letter in enumerate(class_mapping):
         is_letter = lambda df: (df[0] == index)
         emnist_dict[letter] = emnist.loc[is_letter, 1:]
 
     def generator():
+        annotations = []
         base_canvas = Image.fromarray(np.zeros(size))
 
         while True:
@@ -77,7 +79,6 @@ def getGenerator(text=None, size=(500,500)):
         xOffset = 0
         yOffset = space_between_lines
         num_characters_remaining = width//char_size
-        print(num_lines)
 
         while(num_lines > 0):
             while(num_characters_remaining >= 3):
@@ -96,8 +97,12 @@ def getGenerator(text=None, size=(500,500)):
                                  .transpose(Image.TRANSPOSE) \
                                  .resize((char_size,char_size))
                     base_canvas.paste(imgIn, (xOffset, yOffset))
+                    annotations.append((letter, (xOffset, yOffset,
+                                                 xOffset+char_size, yOffset+char_size)))
                     xOffset = xOffset+char_size
 
+                annotations.append(("_", (xOffset, yOffset,
+                                          xOffset+char_size, yOffset+char_size)))
                 xOffset = xOffset + char_size
 
                 num_characters_remaining = num_characters_remaining - len(word) - 1
@@ -106,9 +111,101 @@ def getGenerator(text=None, size=(500,500)):
             yOffset = yOffset + space_between_lines + char_size
             xOffset = 0
             num_characters_remaining = max_letters_per_line
-        return base_canvas
+        return {"img":base_canvas, "annotations":annotations}
 
     return generator
+
+def apply_random_transform(imgObj):
+    img = imgObj["img"]
+    annotations = imgObj["annotations"]
+    def find_coeffs(pa, pb):
+        matrix = []
+        for p1, p2 in zip(pa, pb):
+            matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
+            matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
+
+        A = np.matrix(matrix, dtype=np.float)
+        B = np.array(pb).reshape(8)
+
+        #res = np.dot(np.linalg.inv(A.T * A) * A.T, B)
+        res = np.dot(np.linalg.inv(A), B)
+        return np.array(res).reshape(8)
+
+    width, height = img.size
+    pa = [(0, 0), (width, 0), (width, height), (0, height)]
+
+    sF1 = int(np.random.uniform(0,0.4)*width)
+    sF2 = int(np.random.uniform(0,0.4)*height)
+    pivot = int(np.random.uniform(0, min(sF1,sF2)))
+    left_in = [(-sF1, -sF2-pivot), (width+sF1, -sF2+pivot),
+               (width+sF1, height+sF2-pivot), (-sF1, height+sF2+pivot)]
+    right_in = [(-sF1, -sF2+pivot), (width+sF1, -sF2-pivot),
+                (width+sF1, height+sF2+pivot), (-sF1, height+sF2-pivot)]
+    bottom_in = [(-sF1+pivot, -sF2), (width+sF1-pivot, -sF2),
+                 (width+sF1+pivot, height+sF2), (-sF1-pivot, height+sF2)]
+
+    top_in = [(-sF1-pivot, -sF2), (width+sF1+pivot, -sF2),
+              (width+sF1-pivot, height+sF2), (-sF1+pivot, height+sF2)]
+
+    lc_in = [(-sF1-pivot, -sF2-pivot), (width+sF1, -sF2),
+                  (width+sF1+pivot, height+sF2+pivot), (-sF1, height+sF2)]
+    rc_in = [(-sF1+pivot, -sF2+pivot), (width+sF1, -sF2),
+                  (width+sF1-pivot, height+sF2-pivot), (-sF1, height+sF2)]
+
+    transforms = [left_in, right_in, bottom_in, top_in, lc_in, rc_in]
+
+    left_in = [(-100, 0), (width, 0),
+               (width, height), (0, height)]
+
+    # coeffs = find_coeffs(pa, transforms[np.random.random_integers(0,len(transforms)-1)])
+    coeffs = find_coeffs(pa, left_in)
+    print(coeffs)
+    img = img.transform((width, height), Image.PERSPECTIVE, coeffs, Image.BICUBIC)
+    annotations = list(map(lambda annotation: apply_transform_annotations(coeffs, annotation),
+                      annotations))
+    return {"img":img, "annotations": annotations}
+
+def apply_transform_annotations(coeffs, annotation):
+    a, b, c, d, e, f, g, h = coeffs
+    x_min, y_min, x_max, y_max = annotation[1]
+
+    calc_new_point = lambda p: ((a*p[0]+b*p[1]+c)/(g*p[0]+h*p[1]+1),(d*p[0]+e*p[1]+f)/(g*p[0]+h*p[1]+1))
+
+    bounding_points = [(x_min, y_min), (y_min, x_max), (x_min, y_max), (x_max, y_max)]
+    new_bounding_points = list(map(calc_new_point, bounding_points))
+    print(new_bounding_points)
+    x_min = min(new_bounding_points[:][0])
+    x_max = max(new_bounding_points[:][0])
+    y_min = min(new_bounding_points[:][1])
+    y_max = max(new_bounding_points[:][1])
+
+    #return (annotation[0], (x_min, y_min, x_max, y_max))
+    return (annotation[0], new_bounding_points)
+
+def to_XML(annotations, imgSize):
+    root = ET.Element("annotation")
+    tree = ET.ElementTree(root)
+    folder = ET.SubElement(root, "folder").text = "randyhand_data"
+    size = ET.SubElement(root, "size")
+    width = ET.SubElement(size, "width").text = str(imgSize[0])
+    height = ET.SubElement(size, "height").text = str(imgSize[1])
+    depth = ET.SubElement(size, "depth").text = "3"
+    ET.SubElement(root, "segmented"). text = "0"
+
+    for annotation in annotations:
+        obj = ET.SubElement(root, "object")
+        ET.SubElement(obj, "name").text = annotation[0]
+        ET.SubElement(obj, "pose").text = "Frontal"
+        ET.SubElement(obj, "truncated").text = "0"
+        ET.SubElement(obj, "difficult").text = "0"
+        ET.SubElement(obj, "occluded").text = "0"
+        box = ET.SubElement(obj, "bndbox")
+        ET.SubElement(box, "xmin").text = str(annotation[1][0])
+        ET.SubElement(box, "xmax").text = str( annotation[1][1] )
+        ET.SubElement(box, "ymin").text = str( annotation[1][2] )
+        ET.SubElement(box, "ymax").text = str( annotation[1][3] )
+        
+    return tree
 
 def calculate_line_parameters(size, letter_size):
     """Get random params (that make sense!) for lines to be written
